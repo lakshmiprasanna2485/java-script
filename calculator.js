a2846076df53c26df7a71ec31d2c94678ce4e261
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-12 12:15:40 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let a=25;
+letb=19;
+operation="-"
+switch(operation){
+    case "+":
+        console.log(a+b);
+        break;
+     case "-":
+        console.log(a-b);
+        break;
+     case "%":
+        console.log(a%b);
+        break;
+     case "*":
+        console.log(a*b);
+        break; 
+    default:
+        console.log("invalid");               
+
+}
