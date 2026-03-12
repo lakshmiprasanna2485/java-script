e903d5f955b03c2df00ecca2c777b61079fcb687
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-12 12:17:08 +0530

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,13 @@
+let choice=1;
+if(choice==1){
+    console.log("good");
+}
+else if(choice==2){
+    console.log("average");
+}
+else if(choice==3){
+    console.log("bad");
+}
+else{
+    console.log("ok")
+}
