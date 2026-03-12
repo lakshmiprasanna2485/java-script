967d0db17f743bce91990108f4a712f0aeed877d
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-12 12:16:29 +0530

    Add files via upload

---
+++ trafficlight.js
@@ -0,0 +1,15 @@
+let color=green;
+switch(color){
+    case 1:
+        console.js("green signal to go");
+        break;
+    case 2:
+        console.log("red signal to stop"); 
+        break; 
+    case 3:
+        console.log("yellow signal for get ready to go"); 
+        break;   
+     default:
+        console.log("invalid");   
+
+}
