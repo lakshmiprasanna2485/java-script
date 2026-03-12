b1cb8e991169086ec20ad803379bc32d8bc60f05
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-12 10:50:03 +0530

    Add files via upload

---
+++ marks.js
@@ -0,0 +1,14 @@
+let marks=49;
+if(marks>=33){
+    console.log("grade is c");
+}
+else if(marks>=75){
+    console.log("grade is B");
+
+}
+else if(marks>=85){
+    console.log("grade is A");
+}
+else{
+    console.log(fail);
+}
