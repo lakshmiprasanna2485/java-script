a4670a78ff0846df3aed9ac96407922dceaaab0a
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-12 12:15:18 +0530

    Add files via upload

---
+++ days.js
@@ -0,0 +1,29 @@
+ let days=5;
+ 
+switch(days){
+case 1:
+     console.log("monday")
+     break;
+case 2:
+    console.log("tueday");
+    break;
+case 3:
+    console.log("wednesday");
+    break;
+
+case 4:
+    console.log("thursday");
+    break;
+case 5:
+    console.log("friday");
+    break;
+case 6:
+    console.log("saturday");
+    break;
+ case 7:
+    console.log("sunday");
+    break;   
+
+default:
+    console.log("default");
+}
