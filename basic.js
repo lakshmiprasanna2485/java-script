64a40cba9c7352ea7c1c6149474889238f4470c0
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-13 18:30:46 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,49 @@
+ arr=["Green","Blue"];
+for(let i=0;i<=arr.length;i++){
+    console.log(arr);
+}
+
+
+
+let n=4;
+let i=0;
+while( i<=n){
+     i++;
+    console.log(i)
+}
+
+let count=6;
+while(count>=1){
+    count--;
+    console.log(count)
+}
+let n=10;
+let i=0;
+let sum=0;
+while(i<=n){
+    sum=sum+i
+    i++
+    console.log(sum)
+}
+
+
+let arr=["sun","moon","stars"];
+while(arr!=0){
+    console.log(arr.reverse(arr));
+}
+
+let arr=["sun","moon","stars"];
+for(let i=arr.length-1;i>=0;i--){
+    console.log(arr[i])
+  }
+  
+  
+  let arr="i love greenery"
+  do{
+      console.log(arr);
+}
+while(arr=0){
+    console.log(arr)
+}
+
+
