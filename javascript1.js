1b4118207ce56aa442a2d71b969349ed5aece112
Lakshmi Prasanna <lakshmiprasanna2485@gmail.com>
2026-03-13 19:15:15 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,29 @@
+function isarrshift(arr){
+    return arr.shift();
+}
+let arr=["python","c","c++"];
+console.log(isarrshift(arr));
+console.log(arr)
+
+
+function isarrpush(arr){
+    return arr.push("sql");
+}
+let arr=["python","c","c++"];
+console.log(isarrpush(arr));
+console.log(arr)
+
+function getindex(arr,ele){
+    return arr.indexOf(ele);
+}
+let arr=["maths","java","R"];
+console.log(getindex(arr,"maths"))
+console.log(getindex(arr,"java"))
+console.log(getindex(arr,"R"))
+
+let arr=["pen","pencil"];
+function getreverse(arr,ele){
+    return arr.reverse(ele);
+    console.log(getrevrese(arr));
+    
+}
